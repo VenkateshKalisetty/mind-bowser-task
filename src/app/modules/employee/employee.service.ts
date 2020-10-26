633b52e38ed3b55d19aef16d5d79a15264e97e02
Venkatesh Kalisetty <venky12034@gmail.com>
2020-10-26 21:48:42 +0530

    Service methods added

--- src/app/modules/employee/employee.service.ts
+++ src/app/modules/employee/employee.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IEmployee } from '../shared/models/app.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EmployeeService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  getEmployeesData(id: number): Observable<IEmployee[]> {
+    const endpoint = ``;
+    return this.httpClient.get<IEmployee[]>(endpoint);
+  }
+
+  addEmployee(employee: IEmployee): Observable<IEmployee> {
+    const endpoint = ``;
+    return this.httpClient.post<IEmployee>(endpoint, employee);
+  }
+
+  updateEmployee(employee: IEmployee): Observable<any> {
+    const endpoint = ``;
+    return this.httpClient.post(endpoint, employee);
+  }
+
+  deleteEmployee(id: number): Observable<any> {
+    const endpoint = ``;
+    return this.httpClient.delete(endpoint);
+  }
 }
--- src/app/modules/subscription/subscription.service.ts
+++ src/app/modules/subscription/subscription.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SubscriptionService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  getMonthlySubscription(): Observable<any> {
+    const endpoint = ``;
+    return this.httpClient.get<any>(endpoint);
+  }
 }
